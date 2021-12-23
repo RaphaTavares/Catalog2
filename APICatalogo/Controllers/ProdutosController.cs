80da534cac7d6f7bdb7425a25a9c3b85b60de3de
phael-tavares <raphael_tavares2000@hotmail.com>
2021-12-22 23:52:00 -0300

    Create ProdutosController with Get() method

---
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,26 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APICatalogo.Controllers
+{
+    [Route("/api/[Controller]")]
+    [ApiController]
+    public class ProdutosController: ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext contexto)
+        {
+            _context = contexto;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            return _context.Produto.ToList();
+        }
+    }
+}
