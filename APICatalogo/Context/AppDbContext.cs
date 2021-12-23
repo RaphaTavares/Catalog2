68190f7c6adcb4d34403ae9a41b5245468a28393
phael-tavares <raphael_tavares2000@hotmail.com>
2021-12-22 23:51:28 -0300

    Create migrations to create and populate database

--- APICatalogo/Context/AppDbContext.cs
+++ APICatalogo/Context/AppDbContext.cs
@@ -6,7 +6,9 @@ namespace APICatalogo.Context
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){}
+        public AppDbContext() { }
         public DbSet<Categoria> Categoria { get; set; }
         public DbSet<Produto> Produto { get; set; }
+        public object Produtos { get; internal set; }
     }
 }
