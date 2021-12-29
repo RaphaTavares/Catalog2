2592b13594758882ae50c386c69c673fefaa15b3
phael-tavares <raphael_tavares2000@hotmail.com>
2021-12-29 16:09:26 -0300

    create categoria repository interface and implement concrete class

---
+++ APICatalogo/Repository/CategoriaRepository.cs
@@ -0,0 +1,18 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace APICatalogo.Repository
+{
+    public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+    {
+
+        public CategoriaRepository(AppDbContext contexto): base(contexto) { }
+
+        public IEnumerable<Categoria> GetCategoriasProdutos()
+        {
+            return Get().Include(categoria => categoria.Produtos);
+        }
+    }
+}
