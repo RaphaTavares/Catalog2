9eaa3ad5432fa549754d1f31934a217295476b2b
phael-tavares <raphael_tavares2000@hotmail.com>
2021-12-29 16:09:13 -0300

    Create produto repository interface and implement concrete class

---
+++ APICatalogo/Repository/ProdutoRepository.cs
@@ -0,0 +1,19 @@
+﻿using APICatalogo.Context;
+using APICatalogo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace APICatalogo.Repository
+{
+    public class ProdutoRepository : Repository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(AppDbContext contexto) : base(contexto)
+        { }
+        public IEnumerable<Produto> GetProdutosPorPreco()
+        {
+            return Get().OrderBy(produto => produto.Preco).ToList();
+        }
+    }
+}
